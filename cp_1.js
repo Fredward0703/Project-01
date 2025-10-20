9432cb0f410470777f9cdc17b08ab630ee492384
Federico Montejo <fmontejo@usf.edu>
2025-10-19 21:37:53 -0400

    Added a character counter... also forgot to link the js to html too so did that as well

--- cp_1.js
+++ cp_1.js
@@ -3,3 +3,8 @@ const nameInput = document.getElementById("username");
 const emailInput = document.getElementById("email");
 const comments = document.getElementById("comments");
 const display = document.getElementById("feedback-display");
+const charCount = document.getElementById("charCount");
+
+comments.addEventListener("input", ()=> {
+    charCount.textContent = comments.value.length;
+});
