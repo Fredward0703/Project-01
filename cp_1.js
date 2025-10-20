7d9cdb226b0546aa57d7cd4fc7f2346c54e45dc5
Federico Montejo <fmontejo@usf.edu>
2025-10-19 22:39:42 -0400

    added a mouseout and mouseover event for username

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("feedbackForm");
-const nameInput = document.getElementById("username");
-const emailInput = document.getElementById("email");
+const nameInput = document.getElementById("userNameHeader");
+const emailInput = document.getElementById("emailHeader");
 const comments = document.getElementById("comments");
 const display = document.getElementById("feedback-display");
 const charCount = document.getElementById("charCount");
@@ -8,3 +8,14 @@ const charCount = document.getElementById("charCount");
 comments.addEventListener("input", ()=> {
     charCount.textContent = comments.value.length;
 });
+
+nameInput.addEventListener("mouseover", mouseOver);
+nameInput.addEventListener("mouseout", mouseOut);
+function mouseOver() {
+    nameInput.style.color = "red";
+}
+function mouseOut() {
+    nameInput.style.color = "black";
+}
+
+
