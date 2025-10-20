725c1655b3cce1ef4537ca52e57bd7a261f0b86a
Federico Montejo <fmontejo@usf.edu>
2025-10-19 23:55:58 -0400

    Got the error message down, along with the text of what was written. Could not get the box to display with it

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("feedbackForm");
 const nameInput = document.getElementById("userNameHeader");
+const username = document.getElementById("username");
 const emailInput = document.getElementById("emailHeader");
+const email = document.getElementById("email");
 const comments = document.getElementById("comments");
 const display = document.getElementById("feedback-display");
 const charCount = document.getElementById("charCount");
@@ -20,9 +22,17 @@ function mouseOut() {
 
 form.addEventListener("submit", e =>{
     e.preventDefault();
-    if (!nameInput.value || !emailInput.value || !comments.value) {
+    if (!username.value || !email.value || !comments.value) {
         alert("Please fill out all fields!");
         return;
-    }
-    
+    } 
+    const newDiv = document.createElement("div");
+    newDiv.textContent = `Username:${username.value} Email:${email.value} What you wrote:${comments.value}`;
+    display.appendChild(newDiv);
+
+    username.value = "";
+    email.value = "";
+    comments.value = "";
+    charCount.textContent = "Characters: 0";
 });
+
